f3986b6e138b1857e4a818f4b4d275c994a28e06
Bodddd <73172523+Bodddd@users.noreply.github.com>
2023-11-19 00:32:04 +0200

    Update code_1.2.py

--- code_1.2.py
+++ code_1.2.py
@@ -1,22 +1,19 @@
 import math
 
-# Define the function to calculate Z without numpy
 def calculate_Z(t, x):
     if t > 0:
         return (9 * math.pi * t + 10 * math.cos(x)) / (math.sqrt(t) - abs(math.sin(t))) * math.exp(x)
     else:
         return "t must be greater than 0 to calculate Z."
 
-# Define the piecewise function for f(x) without numpy
 def f(x):
     if x >= 0:
         return 0.5 - 4/abs(x)
     else:
         return (math.sin(x)**2) / (x**2)
 
-# Example usage
-t_value = 1  # Replace with user input
-x_value = 2  # Replace with user input
+t_value = 1  
+x_value = 2  
 
 Z_value = calculate_Z(t_value, x_value)
 f_x_value = f(x_value)
